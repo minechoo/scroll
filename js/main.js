4b3b32d14dcda69e24c677acaff6770cad8b698e
minechoo <choominhae@gmail.com>
2023-06-14 11:58:02 +0900

    반복문

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /*
-Element(DOM).scrollY :브라우저의 스크롤 거리값(동적)
-DOM.offsetTop : 해당요소의
+window.scrollY : 브라우저의 스크롤 거리값 반환 (동적)
+DOM.offsetTop : 해당돔요소의 세로 위치값 반환 (정적)
+window.scrollTo({left: 가로스크롤 위치값, top: 세로스크롤 위치값}) : 해당 위치값으로 스크롤 이동
 */
 
 const secs = document.querySelectorAll('section');
@@ -13,6 +14,15 @@ window.addEventListener('scroll', () => {
 	console.log(secs[1].offsetTop);
 	const scroll = window.scrollY;
 
+	secs.forEach((_, idx) => {
+		if (scroll >= secs[idx].offsetTop + baseline) {
+			for (const el of btns) el.classList.remove('on');
+			btns[idx].classList.add('on');
+			for (const el of secs) el.classList.remove('on');
+			secs[idx].classList.add('on');
+		}
+	});
+	/*
 	if (scroll >= secs[0].offsetTop + baseline) {
 		for (const el of btns) el.classList.remove('on');
 		btns[0].classList.add('on');
@@ -37,6 +47,7 @@ window.addEventListener('scroll', () => {
 		for (const el of secs) el.classList.remove('on');
 		secs[3].classList.add('on');
 	}
+  */
 });
 
 // btns[1].addEventListener('click', () => {
