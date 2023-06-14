e446059dc533ea75c1cd510b84860e3ab662b8f9
minechoo <choominhae@gmail.com>
2023-06-14 12:14:47 +0900

    함수분리

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-/*
-Element(DOM).scrollY :브라우저의 스크롤 거리값(동적)
-DOM.offsetTop : 해당요소의
-*/
-
 const secs = document.querySelectorAll('section');
 const btns = document.querySelectorAll('ul li');
 const speed = 1000;
-const baseline = -window.innerHeight / 3;
 
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', activation);
+
+btns.forEach((btn, idx) => {
+	btn.addEventListener('click', () => moveScroll(idx));
+});
+
+function activation() {
 	const scroll = window.scrollY;
+	const baseline = -window.innerHeight / 2;
 
 	secs.forEach((_, idx) => {
 		if (scroll >= secs[idx].offsetTop + baseline) {
@@ -20,17 +21,12 @@ window.addEventListener('scroll', () => {
 			secs[idx].classList.add('on');
 		}
 	});
-});
+}
 
-btns.forEach((btn, idx) => {
-	let to = secs[idx].offsetTop;
-
-	btn.addEventListener('click', () => {
-		window.scrollTo({ top: to, behavior: 'smooth' });
-		new Anime(window, {
-			prop: 'scroll',
-			value: sece[idx].offsetTop,
-			duration: speed,
-		});
+function moveScroll(idx) {
+	new Anime(window, {
+		prop: 'scroll',
+		value: secs[idx].offsetTop,
+		duration: speed,
 	});
-});
+}
